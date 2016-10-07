30100d0b30338c33fe709df3efe7e454c7810158
Maxi Lucero Correa <mlucerocorrea@outlook.com>
2016-10-07 13:30:20 -0300

    cambios menores en VecinosCoordenadas

--- src/controller/NeighborsCoordinate.java
+++ src/controller/NeighborsCoordinate.java
@@ -52,7 +52,8 @@ public class NeighborsCoordinate implements Iterable<Coordinate> {
 	}
 	
 	@Override
-	public Iterator<Coordinate> iterator() {
+	public Iterator<Coordinate> iterator()
+	{
 		return neighborsCoordinate.iterator();
 	}
 
