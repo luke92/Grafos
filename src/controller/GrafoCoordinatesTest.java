61cc984abba2b659b09fd952d5cbbf191c1006d1
Linkas <Linkas@192.168.1.43>
2016-10-10 02:31:05 -0300

    Unas pruebas mas en GrafoCoordinatesTest

--- src/controller/GrafoCoordinatesTest.java
+++ src/controller/GrafoCoordinatesTest.java
@@ -62,12 +62,10 @@ public class GrafoCoordinatesTest {
 	public void contieneAristaTest()
 	{
 		GrafoCoordinates grafo = instanciaConAristas();
-		Coordinates coordenadas = instanciaCoordenadas();
 		
-		
-		assertTrue( grafo.contieneArista(coordenadas.get(0), coordenadas.get(1)));
-		assertTrue( grafo.contieneArista(coordenadas.get(1), coordenadas.get(2)));
-		assertTrue( grafo.contieneArista(coordenadas.get(2), coordenadas.get(0)));
+		assertTrue( grafo.contieneArista(new Coordinate(-34.52, -58.70), new Coordinate(-70.52, -40.70)));
+		assertTrue( grafo.contieneArista(new Coordinate(-70.52, -40.70), new Coordinate(-45.52, -15.70)));
+		assertFalse( grafo.contieneArista(new Coordinate(-34.52, -58.70), new Coordinate(-24.52, -35.70)));
 	}
 
 	@Test
@@ -76,7 +74,7 @@ public class GrafoCoordinatesTest {
 		GrafoCoordinates grafo = instanciaConAristas();
 		Coordinates coordenadas = instanciaCoordenadas();
 		
-		assertEquals( 4146.364904306314, grafo.getPeso(coordenadas.get(0), coordenadas.get(1)), 1e-4 );
+		assertEquals( 4146.364904306314, grafo.getPeso(new Coordinate(-34.52, -58.70), new Coordinate(-70.52, -40.70)), 1e-4 );
 		assertNotEquals( 2872.12105901625, grafo.getPeso(coordenadas.get(1), coordenadas.get(2)), 1e-4 );
 		assertEquals( 5660.753834519232, grafo.getPeso(coordenadas.get(2), coordenadas.get(3)), 1e-4 );
 	}
